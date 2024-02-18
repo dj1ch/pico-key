76f8209a54cf0662fbf7f863e1c80293fe1403ef
dj1ch <tg.1ch0712@gmail.com>
2024-02-18 14:38:31 -0800

    delete old files

--- attacker/main.py
+++
@@ -1,9 +0,0 @@
-# this is all i have so far, development will be focused on the pico's coding
-
-import os
-
-machine = 192.168.x.x
-
-print(f"SSH'ing into {machine}")
-os.system(f"ssh {machine}")
-

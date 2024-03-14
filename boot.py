21a39f3065c193f3b0af7324760c70fa0b1a088a
dj1ch <tg.1ch0712@gmail.com>
2024-03-13 22:10:00 -0700

    implemented config in boot

--- boot.py
+++ boot.py
@@ -2,6 +2,7 @@
 # licensed under GPL 3.0 
 # boot.py: bootup
 
+import config
 import time
 import microcontroller
 import board
@@ -37,12 +38,13 @@ def load(loops):
 # as of right now it only prints temp and memory available
 def info():
     print("\nBoard info: ")
+    print("Version: " + config.check_version())
     temp = microcontroller.cpu.temperature
     temp_str = str(temp)
-    print(temp_str + " Degrees Celsius")
+    print("Board Temperature: " + temp_str + " Degrees Celsius")
     mem = gc.mem_free()
     mem_str = str(mem)
-    print(mem_str + " Bytes Free")
+    print("Memory: " + mem_str + " Bytes Free")
 
 def startup(): 
     print(coolArt)

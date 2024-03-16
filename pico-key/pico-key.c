d0cdc9584131cbda89b38d467bacc181a77b97ad
dj1ch <tg.1ch0712@gmail.com>
2024-03-16 00:39:16 -0700

    misc and fake usb

--- pico-key/pico-key.c
+++ pico-key/pico-key.c
@@ -1,6 +1,8 @@
 /** 
  * pico-key
  * 
+ * licensed under GPL 3.0
+ * 
  * built by @dj1ch
  */
 
@@ -9,13 +11,20 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <ctype.h>
 
 // sdk
 #include <pico/stdlib.h>
+#include <pico/malloc.h>
 #include <hardware/uart.h>
 #include <hardware/gpio.h>
 
+// led definition
+#define LED_PIN PICO_DEFAULT_LED_PIN
+
 // functions
+int main();
+void boot();
 void buildScript();
 void testScript();
 void read();
@@ -23,7 +32,31 @@ void fakeUSB();
 void misc();
 void options();
 
+void boot() {
+    // boot logo
+    char coolArt[] = "pico-key...";
+    char author[] = "by dj1ch";
+
+    // print this ^^
+    printf("\n%s\n", coolArt);
+    sleep(1);
+    printf("%s\n", author);
+
+    // print memory, cpu freq, etc
+    void* ptr = malloc(10 * sizeof(char));
+
+    if (ptr != NULL) {
+        // print stats
+        malloc_stats();
+
+        free(ptr);
+    } else {
+        printf("\nMemory allocation failed :(\n");
+    }
+}
+
 int main() {
+    boot();
     while (true) {
         printf("\n1. Build Bad USB script\n");
         printf("2. Test Bad USB script\n");
@@ -40,20 +73,25 @@ int main() {
             case 1:
                 buildScript();
                 break;
+
             case 2:
                 testScript();
                 break; 
+
             case 3:
                 fakeUSB();
                 break; 
+
             case 4:
                 misc();
                 break; 
+
             case 5:
                 options();
                 break;
+
             default: 
-                printf("%d: Invalid choice");
+                printf("\n%d: Invalid choice :/\n", choice);
                 break; 
         }
     }
@@ -69,15 +107,67 @@ void testScript() {
 
 }
 
+// crap i gotta build a new compiler for this :/
 void read() {
 
 }
 
 void fakeUSB() {
+    bool led = false;
 
+    printf("\n1. Start LED blinking\n");
+    printf("2. Stop LED blinking\n");
+    printf("\n> ");
+
+    int choiceA;
+    scanf("%d", &choiceA);
+    switch (choiceA) {
+        case 1:
+            if (led) {
+                printf("Already running!\n");
+            } else {
+                led = true;
+                while (led) {
+                    gpio_put(LED_PIN, 1); // led on
+                    sleep_ms(500);
+                    gpio_put(LED_PIN, 0); // led off
+                    sleep_ms(500);
+
+                    if (scanf("%d", &choiceA) == 1 && choiceA == 2) {
+                        led = false;
+                    }
+                }
+            }
+            break;
+
+        case 2:
+            led = false;
+            break;
+
+        default:
+            printf("%d: Invalid choice\n", choiceA);
+    }
 }
+
+
 void misc() {
-    printf("\nNothing here...");
+    printf("\nNothing here, for now... :/\n");
+    printf("\n> ");
+
+    char choiceB[10];
+
+    // remove whitespace
+    fgets(choiceB, sizeof(choiceB), stdin);
+    choiceB[strcspn(choiceB, "\n")] = '\0';
+
+    // uppercase and compare it
+    for (int i = 0; choiceB[i]; i++) {
+        choiceB[i] = toupper(choiceB[i]);
+    }
+
+    if (strcmp(choiceB, "EXIT") == 0) {
+        return; 
+    }
 }
 
 void options() {

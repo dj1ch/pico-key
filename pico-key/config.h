9ff1854794eb27fa347164a5e2f708500bc65933
dj1ch <tg.1ch0712@gmail.com>
2024-03-17 21:49:45 -0700

    additional configs

--- pico-key/config.h
+++ pico-key/config.h
@@ -7,7 +7,11 @@
 #define CONFIG_H
 
 // configuration parameters
+#define LED_PIN PICO_DEFAULT_LED_PIN
 #define PAYLOAD_LOCATION "/payload.dd"
 #define RUN_ON_STARTUP true
+
+// version
+#define VERSION "0.1.0-alpha"
 
 #endif // CONFIG_H

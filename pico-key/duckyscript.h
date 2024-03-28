b45cf805166a7c038bc6637be8c90755e6c92d14
dj1ch <tg.1ch0712@gmail.com>
2024-03-28 00:12:25 -0700

    keys

--- pico-key/duckyscript.h
+++ pico-key/duckyscript.h
@@ -6,89 +6,87 @@
 #ifndef DUCKYSCRIPT_H
 #define DUCKYSCRIPT_H
 
-// keys are yet to be defined
-
 // control keys 
-#define WINDOWS 
-#define GUI 
-#define APP 
-#define MENU 
-#define SHIFT 
-#define ALT 
-#define CONTROL 
-#define CTRL 
+#define WINDOWS         0x08
+#define GUI             0x10
+#define APP             0x20
+#define MENU            0x40
+#define SHIFT           0x80
+#define ALT             0x40
+#define CONTROL         0x20
+#define CTRL            0x20
 
 // arrows
-#define DOWNARROW 
-#define DOWN 
-#define LEFTARROW 
-#define LEFT 
-#define RIGHTARROW 
-#define RIGHT 
-#define UPARROW 
-#define UP 
+#define DOWNARROW       0x51
+#define DOWN            0x51
+#define LEFTARROW       0x50
+#define LEFT            0x50
+#define RIGHTARROW      0x4F
+#define RIGHT           0x4F
+#define UPARROW         0x52
+#define UP              0x52
 
-// other keys?
-#define BREAK 
-#define PAUSE 
-#define CAPSLOCK 
-#define DELETE 
-#define END 
-#define ESC 
-#define ESCAPE 
-#define HOME 
-#define INSERT 
-#define NUMLOCK 
-#define PAGEUP 
-#define PAGEDOWN 
-#define PRINTSCREEN 
-#define ENTER 
-#define SCROLLLOCK 
-#define SPACE 
-#define TAB 
-#define BACKSPACE 
+// other keys
+#define BREAK           0x48
+#define PAUSE           0x48
+#define CAPSLOCK        0x39
+#define DELETE          0x4C
+#define END             0x4D
+#define ESC             0x29
+#define ESCAPE          0x29
+#define HOME            0x4A
+#define INSERT          0x49
+#define NUMLOCK         0x53
+#define PAGEUP          0x4B
+#define PAGEDOWN        0x4E
+#define PRINTSCREEN     0x46
+#define ENTER           0x28
+#define SCROLLLOCK      0x47
+#define SPACE           0x2C
+#define TAB             0x2B
+#define BACKSPACE       0x2A
 
 // abc's
-#define A 
-#define B 
-#define C 
-#define D 
-#define E 
-#define F 
-#define G 
-#define H 
-#define I 
-#define J 
-#define K 
-#define L 
-#define M 
-#define N 
-#define O 
-#define P 
-#define Q 
-#define R 
-#define S 
-#define T 
-#define U 
-#define V 
-#define W 
-#define X 
-#define Y 
-#define Z 
+#define A               0x04
+#define B               0x05
+#define C               0x06
+#define D               0x07
+#define E               0x08
+#define F               0x09
+#define G               0x0A
+#define H               0x0B
+#define I               0x0C
+#define J               0x0D
+#define K               0x0E
+#define L               0x0F
+#define M               0x10
+#define N               0x11
+#define O               0x12
+#define P               0x13
+#define Q               0x14
+#define R               0x15
+#define S               0x16
+#define T               0x17
+#define U               0x18
+#define V               0x19
+#define W               0x1A
+#define X               0x1B
+#define Y               0x1C
+#define Z               0x1D
 
 // f keys
-#define F1 
-#define F2 
-#define F3 
-#define F4 
-#define F5 
-#define F6 
-#define F7 
-#define F8 
-#define F9 
-#define F10 
-#define F11 
-#define F12 
+#define F1              0x3A
+#define F2              0x3B
+#define F3              0x3C
+#define F4              0x3D
+#define F5              0x3E
+#define F6              0x3F
+#define F7              0x40
+#define F8              0x41
+#define F9              0x42
+#define F10             0x43
+#define F11             0x44
+#define F12             0x45
 
 // mouse actions
 typedef enum {

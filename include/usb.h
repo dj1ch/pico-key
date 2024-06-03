68710b5fd11616d24021fd91baea60a2e0ff0ffd
dj1ch <tg.1ch0712@gmail.com>
2024-06-02 23:07:26 -0700

    update the usb spoofing

---
+++ include/usb.h
@@ -0,0 +1,24 @@
+/**
+ * usb.h
+ * usb spoofing headers
+*/
+
+#ifndef USB_H
+#define USB_H
+
+#include "pico/stdio.h"
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "tusb.h"
+#include "tusb_option.h"
+
+class usb {
+public:
+    void fake_usb();
+    void spoof_id();
+private:
+    bool led;
+
+};
+
+#endif // USB_H
